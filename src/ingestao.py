345ecd3f9c63cdafde9bc0c2c66685f93816168a
s3lain <s3lain>
2025-02-23 13:18:50 +0000

    refactor: changin from notebook to py

---
+++ src/ingestao.py
@@ -0,0 +1,25 @@
+import requests
+import json
+import datetime
+
+url = "https://pokeapi.co/api/v2/pokemon?limit=1400"
+
+response = requests.get(url)
+data = response.json()
+data_save = data["results"]
+
+now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+path = f"/Volumes/raw/pokemon/pokemon_raw/pokemons_list/{now}.json"
+
+
+# len(data_save)
+with open(path, "w") as open_file:
+    json.dump(data_save, open_file)
+
+
+
+
+
+
+
